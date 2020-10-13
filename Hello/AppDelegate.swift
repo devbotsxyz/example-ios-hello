7c1bdb6fd7a6c2e90eca019bfc31efd79b53c67d
Stefan Arentz <stefan@arentz.ca>
2020-10-12 23:43:47 -0400

    Update AppDelegate.swift

--- Hello/AppDelegate.swift
+++ Hello/AppDelegate.swift
@@ -26,6 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        
+        let foobar = "not reached"
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
